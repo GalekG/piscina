9b578d6a63412c95ea9e2fdcf7e2a7203d7b9dd1
Kevin Checa <kevincheca2020@itp.edu.co>
2022-10-21 19:00:28 -0500

    ajustes video

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,20 +1,16 @@
-// import playita from "../assets/images/playita1.jpg";
-import PlayitaVideo from "../assets/playita/videos/Playita1.mp4";
-
-// var script = document.createElement("script");
-// script.src = "https://code.jquery.com/jquery-3.6.1.min.js";
-// script.integrity = "sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=";
-// script.crossOrigin = "anonymous";
-// document.getElementsByTagName("head")[0].appendChild(script);
-
 const Index = () => {
   return (
     <div className="div_index">
       <h1>Centro Recreacional La Playita - Mocoa</h1>
+
       <div className="video_index">
-        <video height="600" autoPlay muted loop>
-            <source src={PlayitaVideo} type="video/mp4" />
-        </video>
+        <iframe
+          width="1280"
+          height="720"
+          src="https://www.youtube.com/embed/Fm-ZzLoRWs0?autoplay=1&mute=1&controls=0&showinfo=0"
+          title="CRLAPLAYITA"
+          frameborder="0"
+        ></iframe>
       </div>
       <p>Aquí puedes encontrar toda la información de La Playita.</p>
     </div>
